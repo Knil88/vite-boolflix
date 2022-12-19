9799a2095eb2da0feb65a1ed347ba0786dd87439
Yari <narducciyarigmail.com>
2022-12-19 10:23:19 +0100

    rinnovo di App.vue per modifiche generali

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,124 @@
-<script >
-import axios from "axios"
-import AppSearch from './components/AppSearch.vue';
-import ContentList from './components/ContentList.vue';
-import { store } from './components/store';
-export default{
- name:"App",
-  data() {
-    return {
-      store,
-    }
-  },
-
-// Creiamo il metodo per richiamre la nostra contentList
-
-  methods: {
-    getContent(){
-    
-    // Creiamo la variabile myUrl che richiama store.apiUrl
-    
-    let myUrl = store.apiUrl
-     
-    //Poniamo la condizione che se searchText è diverso  da stringa vuota aggiungiamo ad myUrl lo status che richiam il valore di store.searchText
-
-  
-    if(store.searchText != "" ){
-         myUrl = `https://api.themoviedb.org/3/search/multi?${store.apiKey}&language=it&query=${store.searchText} `
-      }
-      
-    axios
-    
-    //con questo methods richiamiamo myUrl
-    .get(myUrl)
-    
-    //Con then diciamo che tutto è andato per il verso giusto 
-    .then(res =>{
-
-      store.ContentList = res.data.results;
-    })
-
-    //Con catch diciamo che ci sono stati degli errori 
-
-    .catch(err =>{
-      console.log("Errori",err);
-    }
-    )
-   
-  },
-  },
-  //mettiamo tutti i file importati in components
-
-  components: {
-    
-    AppSearch,
-    ContentList,
-    
-    
-  },
-
-
-  //con mounted andiamo a definire la nostra funzione in modo che possa avviarsi
-
-  mounted() {
-        this.getContent();
-      }
-}
-
-</script>
-
 <template>
-    <header>
-      BOOLFLIX
-    <AppSearch  @search="getContent()"/>
-    
-    </header>
-    <main>
-      <ContentList/>
-    </main>
+  <div id="app">
+    <!-- <MyInput/> -->
+    <MyHeader @search="searchFilm"/>
+    <MyMain :searchedFilms="searchedFilms" :searchedSeries="searchedSeries"/>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-  @use'./components/styles/general.scss' as *;
-  @use'./components/styles/partial/variables.scss' as *;
+<script>
+  // importiamo MyInput da "./components/MyInput.vue"
+
+  import MyHeader from "./components/MyHeader.vue"
+  
+  import MyMain from "./components/MyMain.vue"
+  
+  import axios from 'axios'
+  
+  export default {
+    name: 'App',
+    components: {
+      // MyInput
+      MyHeader,
+      MyMain
+    },
+    data(){
+      return{
+        searchedFilms: null,
+        searchedSeries: null,
+        search: null
+      }
+    },
+    methods:{
+        searchFilm(filmName){
+         
+          axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${filmName}&page=1&include_adult=false`)
+         
+          .then((response) => {
+              
+            this.searchedFilms = response.data.results
+             
+            // console.log(this.searchedFilms);
+              
+            for(let i = 0;i < this.searchedFilms.length;i++){
+                  
+              if(this.searchedFilms[i].original_language == "en"){
+                        
+                this.searchedFilms[i].original_language = "gb";
+                  
+              } else if(this.searchedFilms[i].original_language == "ja"){
+                       
+                this.searchedFilms[i].original_language = "jp";
+                  
+              } else if(this.searchedFilms[i].original_language == "hi"){
+                       
+                this.searchedFilms[i].original_language = "in";
+                 
+              } else if(this.searchedFilms[i].original_language == 'cs'){
+                       
+                this.searchedFilms[i].original_language = "cz";
+                  
+              } else if(this.searchedFilms[i].original_language == "ko"){
+                       
+                this.searchedFilms[i].original_language = "kr";
+                 
+              } else if(this.searchedFilms[i].original_language == "sv"){
+                        
+                this.searchedFilms[i].original_language = "ch";
+                 
+              }
+              
+            }
+          
+          })
+          
+          axios.get(`https://api.themoviedb.org/3/search/tv?api_key=d197ccf6d7b8c9f670d6f5e3440c730c&language=en-US&query=${filmName}&page=1&include_adult=false`)
+          
+          .then((response) => {
+          
+            this.searchedSeries = response.data.results
+          
+            console.log(response);
+          
+            for(let i = 0;i < this.searchedSeries.length;i++){
+          
+              if(this.searchedSeries[i].original_language == "en"){
+          
+                this.searchedSeries[i].original_language = "gb";
+          
+              } else if(this.searchedSeries[i].original_language == "ja"){
+          
+                this.searchedSeries[i].original_language = "jp";
+          
+              } else if(this.searchedSeries[i].original_language == "hi"){
+          
+                this.searchedSeries[i].original_language = "in";
+          
+              } else if(this.searchedSeries[i].original_language == 'cs'){
+          
+                this.searchedSeries[i].original_language = "cz";
+          
+              } else if(this.searchedSeries[i].original_language == "ko"){
+          
+                this.searchedSeries[i].original_language = "kr";
+          
+              } else if(this.searchedSeries[i].original_language == "sv"){
+          
+                this.searchedSeries[i].original_language = "ch";
+          
+              }
+          
+            }
+          
+          })
+        },
+
+    },
+  }
+</script>
+
+<style lang="scss">
+  
+@import "./assets/scss/common.scss";
 
 </style>
